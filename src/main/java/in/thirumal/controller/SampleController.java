b095153097ae042c25138333c922df0e139cb67c
Thirumal <m.thirumal@hotmail.com>
2020-03-01 13:56:59 +0530

    Swagger

---
+++ src/main/java/in/thirumal/controller/SampleController.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package in.thirumal.controller;
+
+import java.util.IntSummaryStatistics;
+import java.util.stream.IntStream;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author thirumal
+ *
+ */
+@RestController
+@RequestMapping("/client2")
+public class SampleController {
+
+	@GetMapping("/")
+	public IntSummaryStatistics getStatistic() {
+		return IntStream.range(0, 50).summaryStatistics();
+	}
+}
