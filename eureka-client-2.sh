d72406ed23362694e38a532e4c03fb20f9b01ccf
Thirumal <m.thirumal@hotmail.com>
2020-03-11 23:26:25 +0530

    eureka-client-2.sh

--- eureka-client-2.sh
+++ eureka-client-2.sh
@@ -1,3 +1,15 @@
 mvn clean package -DskipTests=true
 echo 'starting application'
-java -jar target/eureka-client-2-0.0.1-SNAPSHOT.jar
+count=1
+if [ $# -gt 0 ] 
+then
+    count=$1
+    echo "sss" $1
+fi
+
+echo "Total number of instance to start" $count
+for i in $(seq 1 $count);
+do
+ echo "Starting instance " $i " of " $count;
+ nohup java -jar target/eureka-client-2-0.0.1-SNAPSHOT.jar &
+done
