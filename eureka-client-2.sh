c87a1b1463e819e0d541cd86ac36f91282ec9844
Thirumal <m.thirumal@hotmail.com>
2020-03-12 10:04:32 +0530

    No log

--- eureka-client-2.sh
+++ eureka-client-2.sh
@@ -8,12 +8,11 @@ count=1
 if [ $# -gt 0 ] 
 then
     count=$1
-    echo "sss" $1
 fi
 
 echo "Total number of instance to start" $count
 for i in $(seq 1 $count);
 do
  echo "Starting instance " $i " of " $count;
- nohup java -jar target/$appName &
+ nohup java -jar target/$appName >& /dev/null &
 done
