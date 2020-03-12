663a6138d8fd7fb741b2bf36a7c2c806e326a91c
Thirumal <m.thirumal@hotmail.com>
2020-03-12 10:30:02 +0530

    Added header to shell script

--- eureka-client-2.sh
+++ eureka-client-2.sh
@@ -1,3 +1,12 @@
+#! /bin/bash
+#title           :eureka-client-2.sh
+#description     :This script will make a header for a bash script.
+#author		     :Thirumal(m.thirumal@hotmail.com)
+#date            :12/03/2020
+#version         :0.1
+#usage		     :./eureka-client-2.sh 2 in LINUX & sh eureka-client-2.sh 2 in MAC OS
+#notes           :Install Vim
+
 mvn clean package -DskipTests=true
 appName="eureka-client-2-0.0.1-SNAPSHOT.jar"
 echo "Killing existing instances" $appName
